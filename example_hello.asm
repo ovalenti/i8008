cdfdec3f64648b207b41d4a30cc2ceaa09b780c9
Olivier Valentin <valentio@free.fr>
2022-04-20 16:26:32 +0200

    Enhance the HL incrementation to handle 8bits overflow

--- example_hello.asm
+++ example_hello.asm
@@ -1,7 +1,7 @@
 .org 0
 JMP start ; reset vector [0]
 .org 8
-JMP irq   ; IRQ vector [1]
+RET       ; IRQ vector [1]
 
 .org 0x40
 start:
@@ -11,16 +11,24 @@ start:
 	HALT
 	JMP start
 
-irq:
-	RET
-
 ; HL points to the string
 print:
 	LAM
 	CPI 0
 	RTZ
 	OUT/1
-	INL
+	CALL incHL
 	JMP print
+
+incHL:
+	OUT/7
+	LAL
+	ADI 0x01
+	LLA
+	LAH
+	ACI 0x00
+	LHA
+	INP/7
+	RET
 
 hello:.set 'h' 'e' 'l' 'l' 'o' '!' 0xa 0
